5d4889f7b54de8eab369c80af4b5c249ea1888f1
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2022-11-20 20:39:13 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="font-bold text-red-500 text-2xl">Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
