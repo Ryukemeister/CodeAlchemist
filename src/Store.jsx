d537256e45b47dfcc620ff0ecb10f5c9c36b6403
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2022-12-13 19:51:06 +0530

    Added a new value for the convretedCode and a setConvertedCode method to update the converted code

--- src/Store.jsx
+++ src/Store.jsx
@@ -4,6 +4,11 @@ const useStore = create((set) => ({
   code: "",
   codeToBeConverted: "",
   codeToBeExplained: "",
+  convertedCode: "",
+  setConvertedCode: (newValue) =>
+    set(() => ({
+      convertedCode: newValue,
+    })),
   setCodeToBeConverted: (newValue) =>
     set(() => ({
       codeToBeConverted: newValue,
