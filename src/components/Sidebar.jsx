8eb58516e1f69a2e2bcec6d9fb8a1d135bc2b5af
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2022-12-13 19:54:02 +0530

    Chnaged the src link of the cross icon

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 function Sidebar({ handleClick }) {
   return (
     <div className="flex flex-col hamburger-navigation fixed w-[100vw] h-[100vh] translate-x-[-100%] transition-all ease-in-out duration-500 bg-black z-50 md:hidden">
-      <div className="flex w-[100vw] h-[40px] pr-5 transition-all justify-end">
+      <div className="flex w-[100vw] h-[40px] pr-6 transition-all justify-end">
         <button onClick={handleClick}>
           <img
-            className="w-[50px] h-[40px]"
-            src="./public/Cross_Icon.jpg"
+            className="w-[40px] h-[40px]"
+            src="/Cross_Icon.jpg"
             alt="Cross icon"
           />
         </button>
